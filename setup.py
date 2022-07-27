bce9499dc7efc0d1e1ca62cf0c8ad14319ce88b8
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-07-27 16:03:35 +0200

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Get your fucking IP address',
     author='Jorge Alberto Díaz Orozco (Akiel)',
     author_email='diazorozcoj@gmail.com',
-    version="1.0.0",
+    version="1.0.1",
     scripts=["wtfismyip", ],
     install_requires=["requests", "rich", ]
 )
